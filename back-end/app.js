eab0e6973d1c7b9d3184446d25a0d1487d3e5684
HYEIN JU <hyein.ju92@gmail.com>
2019-09-22 21:14:55 +0900

    Add cors middleware

--- back-end/app.js
+++ back-end/app.js
@@ -3,16 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.use(cors());
 if (process.env.NODE_ENV === 'production') {
   app.use(logger('combined'));
 } else {
   app.use(logger('dev'));
 }
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
