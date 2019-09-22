90896a6d8bf1d7af559f936feb247c2f389f937a
HYEIN JU <hyein.ju92@gmail.com>
2019-09-22 18:51:26 +0900

    Refactor code

--- back-end/app.js
+++ back-end/app.js
@@ -8,7 +8,12 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-app.use(logger('dev'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(logger('combined'));
+} else {
+  app.use(logger('dev'));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,53 +1,31 @@
-var express = require('express');
-var axios = require('axios');
-var router = express.Router();
-require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../ctrl/ctrl');
+
+//비동기 함수 래퍼
+const wrap = asyncFn => {
+  return (async (req, res, next) => {
+    try {
+      return await asyncFn(req, res, next)
+    } catch (error) {
+      return next(error)
+    }
+  })  
+}
 
 /* GET comments. */
-router.get('/comments/:id', function (req, res, next) {
+router.get('/comments/:id', wrap(async(req, res, next) => {
   const videoId = req.params.id;
   console.log('videoId: ' + videoId);
- 
-  getData(videoId).then ((comments) => {
-    refineData(comments);
-    //댓글 정제
-    return res.status(200).send();
-    }
-  );
-});
+  const comments = await ctrl.getComments(videoId);
+  if(comments.length === 0) return res.status(200).send();
 
-const refineData = (comments) => {
-  const n = comments.length;
-  var pattern = /[^(가-힣)]|[()]/gi; // 특수문자 제거
-  for(let i = 0; i < n; i++) {
-    comments[i] = comments[i].replace(pattern,' ');
-  }
-  console.log(comments);
-}
+  const nounsArr = await ctrl.refineData(comments);
+  const wordFrequency = await ctrl.getMostFrequentNouns(nounsArr);
 
-const getData = async(videoId) => {
-  let nextPageToken = '';
-  let comments = [];
+  console.log('응답');
+  return res.status(200).send({comments: wordFrequency});
 
-  while(true){
-    const URL = `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=100&textFormat=plainText&videoId=${videoId}&key=${process.env.GOOGLE_API_KEY}`
-    + (nextPageToken? `&pageToken=${nextPageToken}` : '');
-
-    const response = await axios.get(URL);
-    const { totalResults } = response.data.pageInfo;
-    const items = response.data.items;
-
-    for(let i = 0; i < totalResults; i++) {
-      comments.push(items[i].snippet.topLevelComment.snippet.textDisplay);
-    }
-
-    if(!response.data.nextPageToken) {
-      break;
-    } else {
-      nextPageToken = response.data.nextPageToken;
-    };
-  }
-  return comments;
-}
+}));
 
 module.exports = router;
